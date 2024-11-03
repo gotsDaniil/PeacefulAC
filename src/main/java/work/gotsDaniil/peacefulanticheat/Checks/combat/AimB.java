89d25d278cb2d9030e7f55bfbd9e07115ed75e07
gotsDaniil <158559526+gotsDaniil@users.noreply.github.com>
2024-11-03 23:16:50 +0300

    Add files via upload

---
+++ src/main/java/work/gotsDaniil/peacefulanticheat/Checks/combat/AimB.java
@@ -0,0 +1,180 @@
+package work.gotsDaniil.peacefulanticheat.Checks.combat;
+
+import work.gotsDaniil.peacefulanticheat.ConfigManager;
+import work.gotsDaniil.peacefulanticheat.PeacefulAntiCheat;
+import work.gotsDaniil.peacefulanticheat.api.Placeholders;
+import work.gotsDaniil.peacefulanticheat.utils.Alerts.AlertManager;
+import work.gotsDaniil.peacefulanticheat.utils.Discord.DiscordWebhook;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
+
+public class AimB extends PacketListenerAbstract {
+
+    private final ConfigManager configManager;
+    private final DiscordWebhook DiscordWebhook;
+    private final ConcurrentMap<String, Integer> playerViolationCount = new ConcurrentHashMap<>();
+    private final double minAtan2;
+    private final double maxAtan2;
+    private final double minAccuracy;
+    private final double maxAccuracy;
+    private final double minDirectionChange;
+    private final double maxDirectionChange;
+    private final int maxViolations;
+    private final PeacefulAntiCheat plugin;
+    private int newViolations;
+    private final String WEBHOOK_URL;
+
+    public AimB(ConfigManager configManager, PeacefulAntiCheat plugin) {
+        this.configManager = configManager;
+        this.plugin = plugin;
+        this.maxViolations = configManager.AimBMaxViolations();
+        this.minAtan2 = configManager.AimBMinAtan2();
+        this.maxAtan2 = configManager.AimBMaxAtan2();
+        this.minAccuracy = configManager.AimBMinAccuracy();
+        this.maxAccuracy = configManager.AimBMaxAccuracy();
+        this.minDirectionChange = configManager.AimBMinDirectionChange();
+        this.maxDirectionChange = configManager.AimBMaxDirectionChange();
+        this.WEBHOOK_URL = configManager.DISCORD_WEBHOOK_URL();
+        this.DiscordWebhook = new DiscordWebhook(configManager);
+
+        PacketEvents.getAPI().getEventManager().registerListener(this);
+    }
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
+            WrapperPlayClientInteractEntity interactEntity = new WrapperPlayClientInteractEntity(event);
+            User user = event.getUser();
+            UUID playerUUID = user.getUUID();
+            Player player = Bukkit.getPlayer(playerUUID);
+
+            if (player != null) {
+                float pitch = player.getLocation().getPitch();
+                pitch = normalizePitch(pitch);
+                double pitchRadians = Math.toRadians(pitch);
+                double atan2 = Math.atan2(Math.sin(pitchRadians), Math.cos(pitchRadians));
+
+                String playerName = player.getName();
+                int violations = playerViolationCount.getOrDefault(playerName, 0);
+
+                if (atan2 >= minAtan2 && atan2 <= maxAtan2) {
+                    int entityId = interactEntity.getEntityId();
+
+                    Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                        getEntityUUID(entityId, entityUUID -> {
+                            if (entityUUID == null) {
+                                return;
+                            }
+
+                            Bukkit.getScheduler().runTask(plugin, () -> {
+                                Entity targetEntity = Bukkit.getEntity(entityUUID);
+                                if (targetEntity instanceof Player) {
+                                    Player victim = (Player) targetEntity;
+
+                                    Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                                        double accuracy = calculateAim(player, victim);
+                                        double directionChange = calculateDirectionChange(player, victim);
+
+                                        if (accuracy >= minAccuracy && accuracy <= maxAccuracy) {
+                                            if (directionChange >= minDirectionChange && directionChange <= maxDirectionChange) {
+
+                                                event.setCancelled(true);
+
+                                                newViolations = violations + 1;
+                                                playerViolationCount.put(playerName, newViolations);
+
+                                                AlertManager.sendAlert(configManager, player, "AimB", newViolations, maxViolations);
+                                                DiscordWebhook.sendAlert(WEBHOOK_URL, playerName, "AimB", violations, maxViolations);
+
+                                                if (newViolations >= maxViolations) {
+                                                    executePunishment(playerName);
+                                                    playerViolationCount.remove(playerName);
+                                                }
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                        });
+                    });
+                }
+            }
+        }
+    }
+
+    private void getEntityUUID(int entityId, Consumer<UUID> callback) {
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                if (entity.getEntityId() == entityId) {
+                    if (entity instanceof Player) {
+                        Player player = (Player) entity;
+                        UUID playerUUID = player.getUniqueId();
+                        callback.accept(playerUUID);
+                    } else {
+                        callback.accept(null);
+                    }
+                    return;
+                }
+            }
+        }
+        callback.accept(null);
+    }
+
+    private float normalizePitch(float pitch) {
+        pitch = pitch % 360;
+        if (pitch < -90) pitch += 180;
+        if (pitch > 90) pitch -= 180;
+        return pitch;
+    }
+
+    private double calculateDirectionChange(Player attacker, Player victim) {
+        Vector previousDirection = attacker.getLocation().getDirection();
+        Vector currentDirection = victim.getLocation().toVector().subtract(attacker.getLocation().toVector()).normalize();
+        return previousDirection.angle(currentDirection);
+    }
+
+    private double calculateAim(Player attacker, Player victim) {
+        Vector attackerDirection = attacker.getLocation().getDirection();
+        Vector toVictim = victim.getLocation().toVector().subtract(attacker.getLocation().toVector()).normalize();
+        double dotProduct = attackerDirection.dot(toVictim);
+        double angle = Math.toDegrees(Math.acos(dotProduct));
+        double aim = angle / 90.0d;
+        Vector attackerVelocity = attacker.getVelocity();
+        double movementFactor = attackerVelocity.length() / 10.0d;
+        Vector previousDirection = attacker.getLocation().getDirection();
+        double directionChange = previousDirection.angle(attackerDirection);
+        double directionChangeFactor = directionChange / 3.141592653589793d;
+        return aim + movementFactor + directionChangeFactor;
+    }
+
+    private void executePunishment(String playerName) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            String punishment = configManager.AimBPunishment();
+            Player player = Bukkit.getPlayer(playerName);
+            if (player != null) {
+                punishment = Placeholders.replacePlaceholderPlayer(player, punishment);
+
+                AlertManager.sendAlertPunishment(configManager, player, "AimB");
+                DiscordWebhook.sendAlertPunish(WEBHOOK_URL, playerName, "AimB");
+
+                ConsoleCommandSender consoleSender = Bukkit.getServer().getConsoleSender();
+                Bukkit.dispatchCommand(consoleSender, punishment);
+            }
+        });
+    }
+}
---
+++ src/main/java/work/gotsDaniil/peacefulanticheat/Checks/movement/ElytraFly.java
@@ -0,0 +1,178 @@
+package work.gotsDaniil.peacefulanticheat.Checks.movement;
+
+import org.bukkit.command.ConsoleCommandSender;
+import work.gotsDaniil.peacefulanticheat.ConfigManager;
+import work.gotsDaniil.peacefulanticheat.PeacefulAntiCheat;
+import work.gotsDaniil.peacefulanticheat.api.Placeholders;
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import work.gotsDaniil.peacefulanticheat.utils.Alerts.AlertManager;
+import work.gotsDaniil.peacefulanticheat.utils.Discord.DiscordWebhook;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ElytraFly extends PacketListenerAbstract {
+
+    private final PeacefulAntiCheat plugin;
+    private final ConfigManager configManager;
+    private final DiscordWebhook DiscordWebhook;
+    private final ConcurrentMap<UUID, Double> playerHeights = new ConcurrentHashMap<>();
+    private final ConcurrentMap<UUID, Double> playerXPositions = new ConcurrentHashMap<>();
+    private final ConcurrentMap<UUID, Double> playerZPositions = new ConcurrentHashMap<>();
+    private final ConcurrentMap<UUID, Long> fireworkUsage = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, Integer> playerViolationCount = new ConcurrentHashMap<>();
+    private final int maxViolations;
+    private final double height;
+    private final String WEBHOOK_URL;
+
+    public ElytraFly(ConfigManager configManager, PeacefulAntiCheat plugin) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+        this.maxViolations = configManager.ElytraFlyViolations();
+        this.height = configManager.ElytraFlyHeight();
+        this.WEBHOOK_URL = configManager.DISCORD_WEBHOOK_URL();
+        this.DiscordWebhook = new DiscordWebhook(configManager);
+
+        PacketEvents.getAPI().getEventManager().registerListener(this);
+    }
+
+    @Override
+    public void onPacketReceive(PacketReceiveEvent event) {
+
+        Player player = event.getPlayer();
+
+        if (player == null) {
+            return;
+        }
+        UUID playerId = player.getUniqueId();
+
+        double currentHeight = player.getLocation().getY();
+        double currentX = player.getLocation().getX();
+        double currentZ = player.getLocation().getZ();
+
+        if (event.getPacketType() == PacketType.Play.Client.USE_ITEM) {
+            ItemStack item = player.getInventory().getItemInMainHand();
+            if (item.getType() == Material.FIREWORK_ROCKET) {
+                fireworkUsage.put(playerId, System.currentTimeMillis());
+            }
+        }
+
+        if (playerHeights.containsKey(playerId) && event.getPacketType() != PacketType.Play.Client.PLAYER_FLYING &&
+            !isUsingFirework(playerId) && event.getPacketType() != PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT) {
+
+            Double previousHeight = playerHeights.get(playerId);
+            Double previousX = playerXPositions.get(playerId);
+            Double previousZ = playerZPositions.get(playerId);
+
+            String playerName = player.getName();
+            int violations = playerViolationCount.getOrDefault(playerName, 0);
+
+            if (previousHeight != null && previousX != null && previousZ != null) {
+
+                if (Math.abs(currentX - previousX) < 0.000001 && Math.abs(currentZ - previousZ) < 0.000001) {
+
+                    if (currentHeight - previousHeight >= 0.09 && currentHeight - previousHeight < 0.9 && !isPlayerOnSolidBlock(player)) {
+
+                        if (isWearingElytra(player) && !player.isFlying() && !isPlayerOnLadderOrVine(player)) {
+
+                            setBack(player);
+
+                            violations++;
+                            playerViolationCount.put(playerName, violations);
+
+                            AlertManager.sendAlert(configManager, player, "ElytraFly", violations, maxViolations);
+                            DiscordWebhook.sendAlert(WEBHOOK_URL, playerName, "ElytraFly", violations, maxViolations);
+
+                            if (violations >= maxViolations) {
+                                executePunishment(playerName);
+                                playerViolationCount.remove(playerName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        playerHeights.put(playerId, currentHeight);
+        playerXPositions.put(playerId, currentX);
+        playerZPositions.put(playerId, currentZ);
+    }
+
+    private boolean isWearingElytra(Player player) {
+        ItemStack chestplate = player.getInventory().getChestplate();
+        return chestplate != null && chestplate.getType() == Material.ELYTRA;
+    }
+
+    private boolean isPlayerOnSolidBlock(Player player) {
+        Location playerLocation = player.getLocation();
+
+        for (int i = 1; i <= height; i++) {
+
+            Block blockUnderPlayer = playerLocation.clone().subtract(0.3, i-1, 0.3).getBlock();
+            Block blockUnderPlayer1 = playerLocation.clone().subtract(-0.3, i-1, -0.3).getBlock();
+            Block blockUnderPlayer2 = playerLocation.clone().subtract(0.3, i-1, -0.3).getBlock();
+            Block blockUnderPlayer3 = playerLocation.clone().subtract(-0.3, i-1, 0.3).getBlock();
+
+            Material blockType = blockUnderPlayer.getType();
+            Material blockType1 = blockUnderPlayer1.getType();
+            Material blockType2 = blockUnderPlayer2.getType();
+            Material blockType3 = blockUnderPlayer3.getType();
+
+
+            if (blockType.isSolid() || blockType1.isSolid() || blockType2.isSolid() || blockType3.isSolid()
+            || blockType == Material.WATER || blockType1 == Material.WATER || blockType2 == Material.WATER || blockType3 == Material.WATER
+            || blockType == Material.LAVA || blockType1 == Material.LAVA || blockType2 == Material.LAVA || blockType3 == Material.LAVA) {
+              return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPlayerOnLadderOrVine(Player player) {
+
+        Location playerLocation = player.getLocation();
+        Block blockUnderPlayer = playerLocation.clone().subtract(0, 1, 0).getBlock();
+        Material blockType = blockUnderPlayer.getType();
+
+        return blockType == Material.LADDER || blockType == Material.VINE;
+    }
+
+    private boolean isUsingFirework(UUID playerId) {
+        Long lastUsage = fireworkUsage.get(playerId);
+        return lastUsage != null && System.currentTimeMillis() - lastUsage < 3500;
+    }
+
+    private void executePunishment(String playerName) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            String punishment = configManager.NicknamePatternsAPunishment();
+            Player player = Bukkit.getPlayer(playerName);
+            punishment = Placeholders.replacePlaceholderPlayer(player, punishment);
+
+            AlertManager.sendAlertPunishment(configManager, player, "ElytraFly");
+            DiscordWebhook.sendAlertPunish(WEBHOOK_URL, playerName, "ElytraFly");
+
+            ConsoleCommandSender consoleSender = Bukkit.getServer().getConsoleSender();
+            Bukkit.dispatchCommand(consoleSender, punishment);
+        });
+    }
+
+    private void setBack(Player player) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+
+            Location playerLocation = player.getLocation();
+
+            playerLocation.setY(playerLocation.getY() - 2);
+            player.teleport(playerLocation);
+        });
+    }
+}
